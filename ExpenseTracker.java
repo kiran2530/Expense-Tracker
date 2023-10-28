f13d0630812da9ea1978898d4bec952f9e48b12d
kirannikam <nikamsarkar464@gmail.com>
2023-10-29 01:19:38 +0530

    Expense tracker application using java.

---
+++ ExpenseTracker.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+
+class Expense {
+    private String date;
+    private String description;
+    private double amount;
+
+    public Expense(String date, String description, double amount) {
+        this.date = date;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense(date=" + date + ", description=" + description + ", amount=" + amount + ")";
+    }
+}
+class ExpenseStorage {
+    private static final String FILENAME = "expenses.txt";
+
+    public static void saveExpenses(ArrayList<Expense> expenses) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILENAME))) {
+            for (Expense e : expenses) {
+                writer.println(e.getDate() + "," + e.getDescription() + "," + e.getAmount());
+            }
+        } catch (IOException e) {
+            System.out.println("Error saving expenses: " + e.getMessage());
+        }
+    }
+
+    public static ArrayList<Expense> loadExpenses() {
+        ArrayList<Expense> expenses = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILENAME))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                expenses.add(new Expense(parts[0], parts[1], Double.parseDouble(parts[2])));
+            }
+        } catch (IOException e) {
+            System.out.println("Error loading expenses: " + e.getMessage());
+        }
+        return expenses;
+    }
+}
+
+public class ExpenseTracker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Expense> expenses = ExpenseStorage.loadExpenses();
+
+        while (true) {
+            System.out.println("\n===============================");
+            System.out.println("Expense Tracker Menu:");
+            System.out.println("1. Add Expense");
+            System.out.println("2. View Expenses");
+            System.out.println("3. Exit");
+            System.out.println("-------------------------------");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            System.out.println("===============================\n");
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter date (YYYY-MM-DD): ");
+                    String date = scanner.next();
+                    scanner.nextLine();  // Consume newline left over
+                    System.out.print("Enter description: ");
+                    String desc = scanner.nextLine();
+                    System.out.print("Enter amount: ");
+                    double amount = scanner.nextDouble();
+                    expenses.add(new Expense(date, desc, amount));
+                    ExpenseStorage.saveExpenses(expenses);
+                    break;
+                case 2:
+                    System.out.println("\n************************************ EXPENSES RECORD! ************************************");
+                    for (Expense e : expenses) {
+                        System.out.println("\t" + e);
+                    }
+                    System.out.println("\n******************************************************************************************");
+                    break;
+                case 3:
+                    System.out.println("Goodbye!");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please select from the menu.");
+            }
+        }
+    }
+}
